16dfab8b70ca1d50055a3b8b69ca56842db78b5d
Martin Drasar <drasar@ics.muni.cz>
2025-03-07 11:43:24 +0100

    New and revised implementation of actions in the ai-dojo demo.

--- cyst_services/netsecenv_agent/main.py
+++ cyst_services/netsecenv_agent/main.py
@@ -5,7 +5,7 @@ import aiohttp.web_request
 import netaddr
 from aiohttp import web
 from dataclasses import dataclass
-from typing import Tuple, Optional, Dict, Any, Union
+from typing import Tuple, Optional, Dict, Any, Union, List
 from uuid import uuid4
 
 from cyst.api.logic.action import Action
@@ -15,6 +15,7 @@ from cyst.api.environment.message import Request, Response, MessageType, Message
 from cyst.api.environment.resources import EnvironmentResources
 from cyst.api.network.session import Session
 from cyst.api.host.service import ActiveService, ActiveServiceDescription, Service
+from cyst.api.utils.counter import Counter
 from cyst.api.utils.duration import secs
 from cyst.api.utils.singleton import Singleton
 
@@ -100,6 +101,9 @@ class NetSecEnvAgent(ActiveService):
 
         self._actions = {}
 
+        self._sessions = args.get("__sessions", {}) if args else {}
+        self._auths = {}
+
     async def run(self) -> None:
         await self._manager.run()
 
@@ -118,28 +122,131 @@ class NetSecEnvAgent(ActiveService):
     async def execute_action(self, action_name: str, params: Dict) -> Tuple[int, Dict]:
         print(f"Executing action: {action_name} with params: {params}")
 
-        action = self._actions.get(action_name, None)
-        if not action:
+        dst_ip = "127.0.0.1"
+        dst_service = ""
+        action: Action|None = None
+        session: Session|None = None
+        auth: Authorization|None = None
+
+        if action_name.startswith("dojo:"):
+            if action_name == "dojo:exfiltrate_data":
+                action_name = "dojo:direct:exfiltrate_data"
+            action = self._res.action_store.get(action_name)
+            if not action:
+                return 400,  {"message": f"Attempted to run an unknown action '{action_name}'."}
+
+        # External actions
+        # --------------------------------------------------------------------------------------------------------------
+        if action_name == "dojo:scan_network":
+            network = params.get("to_network", None)
+            if not network:
+                return 400, {"message": "Missing parameter 'to_network'."}
+            action.parameters["to_network"].value = netaddr.IPNetwork(network)
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "dojo:find_services":
+            dst_ip = params.get("dst_ip", "")
+            if not dst_ip:
+                return 400, {"message": "Missing parameter 'dst_ip'."}
+            action.parameters["to_network"].value = dst_ip
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "dojo:exploit_server":
+            dst_ip = params.get("dst_ip", "")
+            if not dst_ip:
+                return 400, {"message": "Missing parameter 'dst_ip'."}
+            dst_service = params.get("dst_service", "")
+            if not dst_service:
+                return 400, {"message": "Missing parameter 'dst_service'."}
+            exploit_name = params.get("exploit", "")
+            if not exploit_name:
+                return 400, {"message": "Missing parameter 'exploit'."}
+            exploit = self._res.exploit_store.get_exploit(id=exploit_name)[0]
+            if not exploit:
+                return 400, {"message": f"Exploit with id '{exploit_name}' not available."}
+            action.set_exploit(exploit)
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "dojo:find_data":
+            dst_ip = params.get("dst_ip", "")
+            if not dst_ip:
+                return 400, {"message": "Missing parameter 'dst_ip'."}
+            dst_service = params.get("dst_service", "")
+            if not dst_service:
+                return 400, {"message": "Missing parameter 'dst_service'."}
+            directory = params.get("directory", "")
+            if not directory:
+                return 400, {"message": "Missing parameter 'directory'."}
+            action.parameters["directory"].value = directory
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "dojo:direct:exfiltrate_data":
+            dst_ip = params.get("dst_ip", "")
+            if not dst_ip:
+                return 400, {"message": "Missing parameter 'dst_ip'."}
+            dst_service = params.get("dst_service", "")
+            if not dst_service:
+                return 400, {"message": "Missing parameter 'dst_service'."}
+            path = params.get("path", "")
+            if not path:
+                return 400, {"message": "Missing parameter 'path'."}
+            action.parameters["path"].value = path
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "dojo:block_ip":
+            return 400,  {"message": "Blocking of IPs is not yet implemented."}
+        # --------------------------------------------------------------------------------------------------------------
+        # Internal actions
+        elif action_name == "agent:list_sessions":
+            result = dict()
+            result["sessions"] = []
+            for session in self._sessions.values():
+                result["sessions"].append({
+                    "session id": session.id,
+                    "session start": f"{str(session.start[0])} [{session.start[1]}]",
+                    "session end": f"{str(session.end[0])} [{session.end[1]}]"
+                })
+            return 200, result
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "agent:list_exploits":
+            result = dict()
+            result["exploits"] = []
+            for exploit in self._res.exploit_store.get_exploit():
+                vulnerable_services = []
+                for service in exploit.services.values():
+                    vulnerable_services.append(f"{service.id} ({str(service.min_version)} - {str(service.max_version)})")
+                result["exploits"].append(
+                    {
+                        "exploit id": exploit.id,
+                        "exploit category": exploit.category.name,
+                        "exploit locality": exploit.locality.name,
+                        "vulnerable services": vulnerable_services
+                    }
+                )
+            return 200, result
+        # --------------------------------------------------------------------------------------------------------------
+        elif action_name == "agent:list_auths":
+            result = dict()
+            result["auths"] = []
+            for a in self._auths.values():
+                result["auths"].append({
+                    "auth id": a[0],
+                    "dst ip": a[1],
+                    "dst service": a[2]
+                })
+            return 200, result
+        else:
             return 400, {"message": f"Trying to execute non-existent action: {action_name}"}
 
-        # destination IP and service are mandatory
-        dst_ip = params.get("dst_ip", None)
-        dst_service = params.get("dst_service", None)
+        session_name = params.get("session", "")
+        if session_name:
+            session = self._sessions.get(session_name, None)
+            if not session:
+                return 400, {"message": f"Attempted to use session with ID: {session_name}, however, no such session present."}
 
-        if dst_ip is None:
-            return 400, {"message": "Destination IP for the address not specified"}
+        auth_name = params.get("auth", "")
+        if auth_name:
+            auth_entry = self._auths.get(auth_name, None)
+            if not auth_entry:
+                return 400, {"message": f"Attempted to use auth with ID: {auth_name}, however, no such auth present."}
+            auth=auth_entry[3]
 
-        if dst_service is None:
-            return 400, {"message": "Destination service for the address not specified"}
-
-        for k, v in params.items():
-            if k == "dst_ip" or k == "dst_service":
-                continue
-            if k == "to_network":
-                v = netaddr.IPNetwork(v)
-            action.parameters[k].value = v
-
-        request = self._msg.create_request(dst_ip, dst_service, action)
+        request = self._msg.create_request(dst_ip, dst_service, action, session=session, auth=auth)
 
         f = asyncio.get_running_loop().create_future()
         self._futures[request.id] = f
@@ -151,7 +258,12 @@ class NetSecEnvAgent(ActiveService):
         response: Response = f.result()
         del self._futures[request.id]
 
-        result = {"id": request.id, "status": str(response.status), "content": str(response.content)}
+        if response.auth:
+            auth_id = "authorization_" + str(Counter().get("netsecenv_authorization"))
+            auth_entry = [auth_id, dst_ip, dst_service, response.auth]
+            self._auths[auth_id] = auth_entry
+
+        result = {"id": request.id, "status": str(response.status), "content": str(response.content), "message": str(response)}
 
         return 200, result
 
